04aa8a018f722b6eb6f3c3156d2d3cded9fbfcd1
Masud Rana <md.masudrana.developer@gmail.com>
2026-05-10 00:24:04 +0800

    Update project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png';
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-100 shadow-sm w-[1480px] mx-auto">
       <div className="navbar-start">
         <div className="dropdown md:hidden">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -31,7 +31,7 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn bg-blue-600 text-white">Emergency</a>
       </div>
     </div>
   );
