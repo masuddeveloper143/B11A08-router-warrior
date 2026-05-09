111533de0af8b7bebf9f42b2437df5a4e89bd6ed
Masud Rana <md.masudrana.developer@gmail.com>
2026-05-10 00:04:19 +0800

    Update project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from '../assets/logo.png';
 
 const Navbar = () => {
     return (
       <div className="navbar bg-base-100 shadow-sm">
   <div className="navbar-start">
-    <div className="dropdown">
+    <div className="dropdown md:hidden">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
       </div>
@@ -22,13 +23,13 @@ const Navbar = () => {
         <li><a>Item 3</a></li>
       </ul>
     </div>
-    <div>
-      <img src=".../ass" alt="" />
-      <a className="btn btn-ghost text-xl">daisyUI</a>
+    <div className='flex items-center '>
+      <img className='w-8' src={logo} alt="" />
+      <a className="btn btn-ghost text-2xl font-bold">Phudu</a>
     </div>
   </div>
   <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
+    <ul className="menu menu-horizontal px-1 ">
       <li><a>Item 1</a></li>
       <li>
         <details>
